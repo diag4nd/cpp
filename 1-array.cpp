12cc2d8de4bafc7bac2314831074eabd251e3c15
diag4nd <144257276+diag4nd@users.noreply.github.com>
2023-09-16 21:55:18 +0300

    Add files via upload

---
+++ 1-array.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <queue>                           // Подключение встроенной библиотеки для работы с очередями
+#include <vector>                          // Подключение встроенной библиотеки для работы с векторами
+
+#define SIZE 5
+
+using namespace std;
+
+int main()
+{
+    // Объявление статического массива
+    int m[SIZE];
+
+    // Объявление динамического массива
+    int* mp = new int[SIZE];
+
+    // Объявление очереди
+    queue <int> q;
+
+    // Объявление вектора
+    vector <int> v = {0, 1, 2, 3, 4};
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        m [i] = i;
+        mp[i] = i;
+        q.push(i);
+        cout << v.at(i) << "\t";
+    }
+
+    cout << endl << q.front();
+    delete[] mp;
+    return 0;
+}
---
+++ 2-pointer.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+                                                    // Задание с указателями
+    double** pointer;
+    double** p;
+    double   number = 2.0;
+
+    pointer  = (double**) malloc(sizeof *pointer);  // Выделение памяти под двойной указатель
+    *pointer = (double*)  malloc(sizeof number);
+
+    *pointer = &number;
+    pointer  = &(*pointer);
+
+    cout << **pointer << endl;
+    free(*pointer);        
+                                                    // Освобождение памяти. Конец задания
+    cout << pointer << endl;
+    cout << *pointer << endl;
+    cout << &number << endl;
+
+    return 0;
+}
+
